a2ee3f775751a1bf85131fe381f2dadc2d395f55
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-02-08 19:11:09 +0800

    typo, check more

--- porttest.sh
+++ porttest.sh
@@ -5,9 +5,10 @@ set -ex
 pwd
 
 cd /usr
+la -a ports
 svnlite co svn://svn.freebsd.org/ports/head ports
 
 df -h
 
 cd /usr/ports/editors/libreoffice
-make all-depends-list | awk -F'/' '{print $4"/"$5}' | xarge pkg install -y
+make all-depends-list | awk -F'/' '{print $4"/"$5}' | xargs pkg install -y
