04f67e5715fb51ce933697943428bbeb18708c48
Li-Wen Hsu <lwhsu@lwhsu.org>
2019-02-21 12:37:33 +0000

    Test port with poudriere

--- porttest.sh
+++ porttest.sh
@@ -12,5 +12,26 @@ make update
 
 df -h
 
-cd /usr/ports/editors/libreoffice
-make all-depends-list | awk -F'/' '{print $4"/"$5}' | xargs pkg install -y
+#cd /usr/ports/editors/libreoffice
+#make all-depends-list | awk -F'/' '{print $4"/"$5}' | xargs pkg install -y
+
+echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+
+poudriere jail -c -j jail -v `uname -r`
+poudriere ports -c -f none -m null -M /usr/ports
+
+# use an easy port to bootstrap pkg repo
+poudriere bulk -t -j jail net/nc
+
+cd /usr/ports
+cd editors/libreoffice
+PWD=`pwd -P`
+PORTDIR=`dirname ${PWD}`
+PORTDIR=`dirname ${PORTDIR}`
+make all-depends-list | sed -e "s,${PORTDIR}/,," | xargs sudo pkg fetch -y -o pkgs
+mv pkgs/All/* /usr/local/poudriere/data/packages/jail-default/.latest/all/
+rm -fr pkgs
+
+poudriere testport -j jail editors/libreoffice
